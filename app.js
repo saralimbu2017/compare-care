fdc3f252bd90a7c4f79ae2b3054b1f6b72938110
sara limbu <limbusaraswati@gmail.com>
2019-05-21 10:36:47 +1000

    Setting the frontend environment

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express =  require("express");
 const app = express();
 const port = 8080;
 const _ = require("underscore");
+const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.userName);
+})
 app.get("/",(req,res) => {
-  res.send("hello");
+  //res.send("hello");
+  res.render('index');
 })
 
 app.listen(port,() => {
